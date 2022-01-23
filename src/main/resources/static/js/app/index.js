d6a81cc4ae708afcd4e58375292f350a015c6d43
천영환 <zing0982@naver.com>
2022-01-24 02:05:09 +0900

    commit

--- src/main/resources/static/js/app/index.js
+++ src/main/resources/static/js/app/index.js
@@ -1,5 +1,5 @@
 var main = { //나중에  a.js란게 추가될 경우 브라우저의 스콮느느 공용공간으로 쓰이기때문에 나중에 로딩된 js의 init, save가 먼저로딩된 js의 function을 덮어씀.
-            //여러사람이 참여하는 프로젝트는 중복된 함수이름은 발생하기때문에 피하려고 index.js만의 유효범위를 만들어 사용 > var index이란 객체 만들어서 안에서 필요한 모든 fucntion 선언
+            //여러사람이 참여하는 프로젝.트는 중복된 함수이름은 발생하기때문에 피하려고 index.js만의 유효범위를 만들어 사용 > var index이란 객체 만들어서 안에서 필요한 모든 fucntion 선언
     init : function () {
         var _this = this;
         $('#btn-save').on('click', function () {
